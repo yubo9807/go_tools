b2d4ecd4f2fa5cf8e4575cf5a17f24a9f0db2689
yangyb <yangyb@boyachain.cn>
2023-01-24 15:45:33 +0800

    feat: proxy 配置生成

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module command
 
 go 1.18
 
-require (
-	github.com/0xAX/notificator v0.0.0-20220220101646-ee9b8921e557
-)
+require github.com/0xAX/notificator v0.0.0-20220220101646-ee9b8921e557
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
--- src/proxy/main.go
+++ src/proxy/main.go
@@ -6,27 +6,69 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 	"strconv"
 	"strings"
+
+	"gopkg.in/yaml.v2"
 )
 
-type proxyType struct {
-	prefix string
-	target string
+type ProxyType struct {
+	Prefix string
+	Target string
 }
 
-var proxyList = []proxyType{}
+type ConfigType struct {
+	Https bool
+	Port  int
+	Proxy []ProxyType
+}
+
+var config ConfigType
 
 func init() {
-	proxyList = append(proxyList, proxyType{"/", "http://hpyyb.cn"})
-	proxyList = append(proxyList, proxyType{"/api", "http://hicky.hpyyb.cn"})
+	configFile := "./proxy.yml"
+	data, err := os.ReadFile(configFile)
+	if err != nil {
+		os.Create(configFile)
+		template := `https: false  # 开发中，暂不支持
+port: 9000
+proxy:
+  - prefix: "/"
+    target: "http://hpyyb.cn"
+  - prefix: "/api"
+    target: "http://hicky.hpyyb.cn"
+
+`
+		os.WriteFile(configFile, []byte(template), 0777)
+		data, _ = os.ReadFile(configFile)
+	}
+
+	if err := yaml.Unmarshal([]byte(data), &config); err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	slice := make([]*url.URL, 0)
+	for _, val := range config.Proxy {
+		addr, _ := url.Parse(val.Target)
+		slice = append(slice, addr)
+	}
+	proxy := newMultipleHostsReverseProxy(slice)
+
+	port := ":" + strconv.Itoa(utils.Server.PortResult(config.Port))
+	fmt.Println("http://localhost" + port)
+	if err := http.ListenAndServe(port, proxy); err != nil {
+		fmt.Println(err.Error())
+	}
 }
 
 func newMultipleHostsReverseProxy(targets []*url.URL) *httputil.ReverseProxy {
 	director := func(req *http.Request) {
 		var target *url.URL
-		for i, val := range proxyList {
-			if strings.HasPrefix(req.RequestURI, val.prefix) {
+		for i, val := range config.Proxy {
+			if strings.HasPrefix(req.RequestURI, val.Prefix) {
 				target = targets[i]
 				continue
 			} else {
@@ -38,19 +80,4 @@ func newMultipleHostsReverseProxy(targets []*url.URL) *httputil.ReverseProxy {
 		req.Host = target.Host
 	}
 	return &httputil.ReverseProxy{Director: director}
-}
-
-func main() {
-	slice := make([]*url.URL, 0)
-	for _, val := range proxyList {
-		addr, _ := url.Parse(val.target)
-		slice = append(slice, addr)
-	}
-	proxy := newMultipleHostsReverseProxy(slice)
-
-	port := ":" + strconv.Itoa(utils.Server.PortResult(9000))
-	fmt.Println("http://localhost" + port)
-	if err := http.ListenAndServe(port, proxy); err != nil {
-		fmt.Println(err.Error())
-	}
 }

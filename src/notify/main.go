c5bacdd314c64f57ad3dab388c4b10210ce921c1
yangyb <yangyb@boyachain.cn>
2024-08-07 15:37:15 +0800

    feat: Notification sound

--- src/notify/main.go
+++ src/notify/main.go
@@ -3,11 +3,11 @@
 package main
 
 import (
+	"command/src/utils"
 	"fmt"
 	"os"
+	"os/exec"
 	"time"
-
-	"command/src/utils"
 
 	"github.com/0xAX/notificator"
 	"gopkg.in/yaml.v2"
@@ -21,11 +21,16 @@ type NotifyType struct {
 	Text  string
 }
 type ConfigType struct {
-	Times map[string]NotifyType
+	Interval  int
+	SoundFile string `yaml:"soundFile"`
+	Times     map[string]NotifyType
 }
 
 var config ConfigType
-var template = `times:
+var template = `interval: 30  # 定时器间隔，秒
+soundFile: '/System/Library/Sounds/Purr.aiff'  # 通知声音文件位置
+
+times:
   '09:00':
     title: '打卡打卡'
   '11:28':
@@ -47,14 +52,13 @@ func init() {
 		data, _ = os.ReadFile(configFile)
 	}
 
-	fmt.Println(notify)
 	if err := yaml.Unmarshal([]byte(data), &config); err != nil {
 		panic(err.Error())
 	}
 }
 
 func main() {
-	timer := time.NewTicker(time.Duration(time.Second) * 30)
+	timer := time.NewTicker(time.Duration(config.Interval) * time.Second)
 	fmt.Println("启动成功：" + utils.Date.DateFormater(time.Now(), ""))
 	for range timer.C {
 		nowDate := utils.Date.DateFormater(time.Now(), "hh:mm")
@@ -62,6 +66,8 @@ func main() {
 			val, ok := config.Times[nowDate]
 			if ok {
 				notify.Push(val.Title, val.Text, "", notificator.UR_CRITICAL)
+				cmd := exec.Command("afplay", config.SoundFile)
+				cmd.Run()
 			}
 			record = nowDate
 		}
